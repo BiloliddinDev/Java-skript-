796e578a0f84cf2f5d6809f9b58b0e54417bb9f7
bilol <bilol09876@gmail.com>
2022-08-13 18:09:24 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let sifat = prompt("Najot talimda sifat");
+
+let joylashuv = prompt("Najot Talimda qaysi filyalda taqsil olasiz");
+
+let yonalish = prompt("Najot talimida qaysi yonalishda taqsil olasiz ");
+
+let sabab = prompt("Najot Talimga kelishingizga nma sabab bolgan");
+
+let qiynchlik = prompt("Oqsh jarayonida duch kelgan qiynchlikar");
+
+let kelajak = prompt("Kelajakdagi Maqsadlaringiz");
+
+console.log(
+  `Sifat:${sifat}\n Joylashuv:${joylashuv} \n Yonalish:${sabab} \n Qiynchlik:${qiynchlik} \n Kelajak ${kelajak} \n`
+);
